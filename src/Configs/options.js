876b9837442748057108eaa23f744776edd29f9a
John James <john.usa.james@gmail.com>
2023-01-26 18:09:18 +0000

    feat(select): define static options

---
+++ src/Configs/options.js
@@ -0,0 +1,32 @@
+export const dates = [
+  {
+    value: 1,
+    label: "1",
+  },
+  {
+    value: 2,
+    label: "2",
+  },
+];
+
+export const months = [
+  {
+    value: 1,
+    label: "January",
+  },
+  {
+    value: 2,
+    label: "February",
+  },
+];
+
+export const years = [
+  {
+    value: 2000,
+    label: "2000",
+  },
+  {
+    value: 2001,
+    label: "2001",
+  },
+];

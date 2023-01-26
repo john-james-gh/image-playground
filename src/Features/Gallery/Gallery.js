af073f56f39d003be26d839910979ba71048fdad
John James <john.usa.james@gmail.com>
2023-01-26 18:08:05 +0000

    feat(gallery): code it out and define styles

---
+++ src/Features/Gallery/Gallery.js
@@ -0,0 +1,71 @@
+import { Fragment, useState } from "react";
+import useGallery from "./Hooks/useGallery";
+
+import { Pagination } from "../../Navigation";
+import { Modal } from "../../DataDisplay";
+import * as S from "./styles";
+
+const Gallery = () => {
+  const [current, setCurrent] = useState(1);
+  const [isOpen, setIsOpen] = useState({});
+
+  const { data, isLoading, error } = useGallery({
+    path: `images?page=${current}`,
+  });
+
+  if (error) return <h2>{error}</h2>;
+
+  return (
+    <>
+      <h2>Gallery</h2>
+
+      <S.UnorderedList>
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          data.items.map((d, index) => (
+            <Fragment key={d.url}>
+              <li>
+                <S.Button
+                  aria-haspopup={true}
+                  aria-label="open modal photo"
+                  aria-describedby={d.alt_description}
+                  type="button"
+                  onClick={() => setIsOpen({ [index]: true })}
+                >
+                  <S.Image
+                    alt={d.alt_description}
+                    src={`${d.url}.jpg`}
+                    height="100px"
+                    width="100px"
+                    loading="lazy"
+                  />
+                </S.Button>
+              </li>
+
+              {isOpen[index] && (
+                <Modal onClose={() => setIsOpen({ [index]: false })}>
+                  <S.Modalmage
+                    loading="lazy"
+                    src={`${d.url}.jpg`}
+                    alt={d.alt_description}
+                  />
+                </Modal>
+              )}
+            </Fragment>
+          ))
+        )}
+      </S.UnorderedList>
+
+      <footer>
+        <Pagination
+          current={current}
+          onChange={setCurrent}
+          total={data?.total || 0}
+        />
+      </footer>
+    </>
+  );
+};
+
+export default Gallery;
---
+++ src/Features/Gallery/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const BaseImage = styled.img`
+  display: block;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+`;
+
+export const Image = styled(BaseImage)`
+  object-fit: cover;
+`;
+
+export const Modalmage = styled(BaseImage)`
+  object-fit: contain;
+`;
+
+export const Button = styled.button`
+  width: 100%;
+  height: 180px;
+  padding: var(--smallSpacing);
+`;
+
+export const UnorderedList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: grid;
+  grid-template-columns: var(--twoColumn);
+  grid-gap: var(--smallSpacing);
+  overflow-y: scroll;
+  height: 62vh;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: var(--threeColumn);
+  }
+
+  @media screen and (min-width: 1200px) {
+    grid-template-columns: var(--fourColumn);
+  }
+`;

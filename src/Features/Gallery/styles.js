40699bec7cf8a4c9b7a172357a27d415d03b65cd
John James <john.usa.james@gmail.com>
2023-01-28 17:59:34 +0000

    style(gallery): remove cursor

--- src/Features/Gallery/styles.js
+++ src/Features/Gallery/styles.js
@@ -4,11 +4,11 @@ const BaseImage = styled.img`
   display: block;
   width: 100%;
   height: 100%;
-  cursor: pointer;
 `;
 
 export const Image = styled(BaseImage)`
   object-fit: cover;
+  cursor: pointer;
 `;
 
 export const Modalmage = styled(BaseImage)`
